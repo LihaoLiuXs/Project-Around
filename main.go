b16cdb2134bf6fcf8438cb071c091d7da916c7ce
Lihao Liu <liulihao731@gamil.com>
2019-02-01 22:10:11 -0500

    second commit

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"reflect"
 	"strconv"
 	"strings"
+
+	"cloud.google.com/go/storage"
 
 	"github.com/google/uuid"
 	elastic "gopkg.in/olivere/elastic.v3"
@@ -22,7 +26,9 @@ const (
 	//PROJECT_ID = "around-230220"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL when I deploy it to cloud.
-	ES_URL = "http://35.202.21.239:9200"
+	ES_URL = "http://104.197.4.169:9200"
+
+	BUCKET_NAME = "post-images-230220"
 )
 
 type Location struct {
@@ -36,6 +42,7 @@ type Post struct {
 	User     string   `json:"user"`
 	Message  string   `json:"message"`
 	Location Location `json:"location"`
+	Url      string   `json:"url"`
 }
 
 func main() {
@@ -79,21 +86,107 @@ func main() {
 }
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
-	// Parse from body of request to get a json object.
-	fmt.Println("Received one post request")
-	decoder := json.NewDecoder(r.Body)
-	var p Post
-	if err := decoder.Decode(&p); err != nil {
-		panic(err)
-		return
+	//// Parse from body of request to get a json object.
+	//fmt.Println("Received one post request")
+	//decoder := json.NewDecoder(r.Body)
+	//var p Post
+	//if err := decoder.Decode(&p); err != nil {
+	//	panic(err)
+	//	return
+	//}
+
+	//fmt.Fprintf(w, "Post received: %s\n", p.Message)
+
+	//uid := uuid.New()
+	//id := uid.String()
+	//// Save to ES.
+	//saveToES(&p, id)
+
+	//new postHandler for new data content
+	//set header
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+	// 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
+	// After I call ParseMultipartForm, the file will be saved in the server memory with maxMemory size.
+	// If the file size is larger than maxMemory, the rest of the data will be saved in a system temporary file.
+	r.ParseMultipartForm(32 << 20)
+
+	// Parse from form data.
+	fmt.Printf("Received one post request %s\n", r.FormValue("message"))
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+
+	p := &Post{
+		User:    "1111",
+		Message: r.FormValue("message"),
+		Location: Location{
+			Lat: lat,
+			Lon: lon,
+		},
 	}
 
-	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+	uuid := uuid.New()
+	id := uuid.String()
 
-	uid := uuid.New()
-	id := uid.String()
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Image is not available", http.StatusInternalServerError)
+		fmt.Printf("Image is not available %v.\n", err)
+		panic(err)
+	}
+	defer file.Close()
+
+	ctx := context.Background()
+
+	_, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
+	if err != nil {
+		http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+		fmt.Printf("GCS is not setup %v\n", err)
+		panic(err)
+	}
+
+	// Update the media link after saving to GCS.
+	p.Url = attrs.MediaLink
+
 	// Save to ES.
-	saveToES(&p, id)
+	saveToES(p, id)
+
+	// Save to BigTable.
+	//saveToBigTable(p, id)
+}
+
+// Save an image to GCS.
+func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer client.Close()
+
+	bucket := client.Bucket(bucketName)
+	// Next check if the bucket exists
+	if _, err = bucket.Attrs(ctx); err != nil {
+		return nil, nil, err
+	}
+
+	obj := bucket.Object(name)
+	wc := obj.NewWriter(ctx)
+	if _, err := io.Copy(wc, r); err != nil {
+		return nil, nil, err
+	}
+	if err := wc.Close(); err != nil {
+		return nil, nil, err
+	}
+
+	if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, nil, err
+	}
+
+	attrs, err := obj.Attrs(ctx)
+	fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
+	return obj, attrs, err
 }
 
 // Save a post to ElasticSearch
